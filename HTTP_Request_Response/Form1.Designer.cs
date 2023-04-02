3a5ce8431cce2068c7cd805c782d8e90cdd9dd7e
Sergey Evseev <sevseev@gmail.com>
2023-04-02 15:54:12 +0300

    initial code with comments

--- HTTP_Request_Response/Form1.Designer.cs
+++ HTTP_Request_Response/Form1.Designer.cs
@@ -30,7 +30,7 @@
         {
             this.URL = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.sendButton = new System.Windows.Forms.Button();
             this.proxyAddr = new System.Windows.Forms.TextBox();
             this.proxyUser = new System.Windows.Forms.TextBox();
             this.proxyPassword = new System.Windows.Forms.TextBox();
@@ -58,14 +58,15 @@
             this.label1.TabIndex = 1;
             this.label1.Text = "URL:";
             // 
-            // button1
+            // sendButton
             // 
-            this.button1.Location = new System.Drawing.Point(465, 21);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "SEND";
-            this.button1.UseVisualStyleBackColor = true;
+            this.sendButton.Location = new System.Drawing.Point(465, 21);
+            this.sendButton.Name = "sendButton";
+            this.sendButton.Size = new System.Drawing.Size(75, 23);
+            this.sendButton.TabIndex = 2;
+            this.sendButton.Text = "SEND";
+            this.sendButton.UseVisualStyleBackColor = true;
+            this.sendButton.Click += new System.EventHandler(this.sendButton_Click);
             // 
             // proxyAddr
             // 
@@ -156,11 +157,11 @@
             this.Controls.Add(this.proxyPassword);
             this.Controls.Add(this.proxyUser);
             this.Controls.Add(this.proxyAddr);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.sendButton);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.URL);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "SEND REQUEST";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -170,7 +171,7 @@
 
         private System.Windows.Forms.TextBox URL;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button sendButton;
         private System.Windows.Forms.TextBox proxyAddr;
         private System.Windows.Forms.TextBox proxyUser;
         private System.Windows.Forms.TextBox proxyPassword;
--- HTTP_Request_Response/Form1.cs
+++ HTTP_Request_Response/Form1.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +17,43 @@ namespace HTTP_Request_Response
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void sendButton_Click(object sender, EventArgs e)
+        {
+            //creates a new HttpWebRequest object and initializes it with the URL
+            //specified in the URL TextBox control
+            HttpWebRequest req = (HttpWebRequest)HttpWebRequest.Create(URL.Text);
+
+            //sets the Method property of the HttpWebRequest object to "GET"
+            //request will use the HTTP GET method to retrieve the resource
+            req.Method = "GET";
+
+            if(ifProxy.Checked)
+            {
+                // initializes a new WebProxy object
+                WebProxy proxy = new WebProxy(proxyAddr.Text);
+
+                //credentials are passed as a NetworkCredential object
+                proxy.Credentials = new NetworkCredential(proxyUser.Text,
+                    proxyPassword.Text);
+                //setting proxy property of a web request object (req) to the WebProxy object (proxy)
+                req.Proxy = proxy;
+
+            }
+            //calling GetResponse() method on the HttpWebRequest object (req) 
+            //method GetResponse() returns a HttpWebResponse object (rez)
+            //that contains the response from the server
+            HttpWebResponse rez = (HttpWebResponse)req.GetResponse();
+
+            //creating a StreamReader object (sr) and initializes it
+            //with the response stream from the HttpWebResponse object
+            StreamReader sr = new StreamReader(rez.GetResponseStream(), 
+                Encoding.Default);
+            //reads the entire response from the server as a string using the ReadToEnd()
+            //method on the StreamReader object (sr) and sets the resulting string
+            //as the text of a control named 'response'
+            response.Text = sr.ReadToEnd();
         }
     }
 }
